91f2db33ff7724f4c3ea341913d431110453d726
Antonia Siu <mail@antoniasiu.co.uk>
2017-09-14 13:20:54 +0100

    Attempt to reconnect if there is an error

--- src/App.vue
+++ src/App.vue
@@ -48,6 +48,7 @@ export default {
         this.message = '';
         this.switchView('game');
       } else {
+        this.$socket.connect();
         this.message = 'unable to connect to server';
       }
     },
